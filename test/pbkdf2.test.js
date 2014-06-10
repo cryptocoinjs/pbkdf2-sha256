cd112b7a22b4a017e9b19723727f7099ba60b518
JP Richardson <jprichardson@gmail.com>
2014-06-10 00:53:35 -0500

    setting up invalid tests

--- test/pbkdf2.test.js
+++ test/pbkdf2.test.js
@@ -5,19 +5,31 @@ var fixtures = require('./fixtures/pbkdf2')
 
 describe('pbkdf2', function() {
 
-  fixtures.valid.forEach(function(f) {
-    it('should compute ' + f.description, function() {
-      if (f.skip) return //skip the one with high # iterations
-      var key = f.key
-      var salt = f.salt
+  describe('> when valid', function() {
+    fixtures.valid.forEach(function(f) {
+      it('should compute ' + f.description, function() {
+        if (f.skip) return //skip the one with high # iterations
+        var key = f.key
+        var salt = f.salt
 
-      if (f.type) {
-        key = new Buffer(f.key, f.type)
-        salt = new Buffer(f.salt, f.type)
-      }
+        if (f.type) {
+          key = new Buffer(f.key, f.type)
+          salt = new Buffer(f.salt, f.type)
+        }
 
-      var res = pbkdf2(key, salt, f.iterations, f.keylen)
-      assert.equal(res.toString('hex'), f.result)
+        var res = pbkdf2(key, salt, f.iterations, f.keylen)
+        assert.equal(res.toString('hex'), f.result)
+      })
+    })
+  })
+
+  describe('> when invalid', function() {
+    fixtures.invalid.forEach(function(f) {
+      it('should throw ' + f.description, function() {
+        assert.throws(function() {
+          var res = pbkdf2(f.key, f.salt, f.iterations, f.keylen)
+        }, new RegExp(f.description, 'i'))
+      })
     })
   })
 })
